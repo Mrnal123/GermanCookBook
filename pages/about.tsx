98193fad51a5435cceef1373ef413b8ee258a8dc
HoloBrand <user@example.com>
2025-04-19 17:47:28 +0530

    Update About page and add new image

--- pages/about.tsx
+++ pages/about.tsx
@@ -57,7 +57,7 @@ const AboutPage = () => {
     {
       name: 'Prahshansha',
       role: 'Teammitglied',
-      image: '/images/prashansha.jpg'
+      image: '/images/prashansa.jpg'
       
     }
   ];
